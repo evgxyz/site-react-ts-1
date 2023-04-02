03ba1973b82cc7073264d53ca0d4d94b2425952d
Evgeniy <evgeniy8086@ya.ru>
2023-04-02 23:30:59 +0300

    v8.1

--- src/units/utils.tsx
+++ src/units/utils.tsx
@@ -9,4 +9,4 @@ export function mergeObj<T extends object, T2 extends object>(obj: T, addObj: T2
 
 export function addObj<T extends object, T2 extends object>(obj: T, addObj: T2) {
   return Object.assign(obj, addObj) as (T & T2);
-}
+} 
